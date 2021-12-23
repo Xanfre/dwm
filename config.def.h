259c86abca227c03d3f27a5e916eb9fed0ee7f7e
Xanfre <xanfrein@gmail.com>
2021-12-23 02:43:52 -0600

    Tweak mfact

--- config.def.h
+++ config.def.h
@@ -46,7 +46,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -94,8 +94,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,                    focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,                    incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_o,                    incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,                    setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,                    setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_h,                    setmfact,       {.f = -0.01} },
+	{ MODKEY,                       XK_l,                    setmfact,       {.f = +0.01} },
 	{ MODKEY|ShiftMask,             XK_Tab,                  zoom,           {0} },
 	{ MODKEY,                       XK_equal,                incrgaps,       {.i = +1 } },
 	{ MODKEY,                       XK_minus,                incrgaps,       {.i = -1 } },
